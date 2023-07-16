c959ea66c05b04e3c40124ab7bbbc647201fbde4
Godwin45 <71969710+Godwin45@users.noreply.github.com>
2023-07-16 18:58:05 +0300

    data validation done

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -8,5 +8,10 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
 
 
--- src/textSummarizer/pipeline/stage_02_data_validation.py
+++ src/textSummarizer/pipeline/stage_02_data_validation.py
@@ -1,5 +1,5 @@
 from textSummarizer.config.configuration import ConfigurationManager
-from textSummarizer.conponents.data_validation import DataValiadtion
+from textSummarizer.components.data_validation import DataValiadtion
 from textSummarizer.logging import logger
 
 
